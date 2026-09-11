762bf4b27c4b826b56e8fbae7a12171a6d68db2c
Rudra Pratap <rudrapratap17003@gmail.com>
2026-09-11 15:58:34 +0530

    fix(app): remove invalid useLanguage hook call outside LanguageProvider to prevent blank screen

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { useLanguage } from './context/LanguageContext'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { AuthProvider, useAuth } from './context/AuthContext'
 import { ScenarioProvider } from './context/ScenarioContext'
@@ -123,7 +122,6 @@ function AppRoutes() {
 }
 
 export default function App() {
-  const { t } = useLanguage()
   return (
     <LanguageProvider>
       <AuthProvider>
